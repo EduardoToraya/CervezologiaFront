00c29bfd7066ca9a67f2ada03fb51e566a12666f
Diego Martinez <diegomtz1998@hotmail.com>
2020-04-15 12:33:04 -0500

    Add filtering by origin, brewery and style

--- src/components/Beers/Beers.jsx
+++ src/components/Beers/Beers.jsx
@@ -1,54 +1,36 @@
-import { Card, Empty, Icon, List, Typography, Layout, message, 
-  Button, Row, Col, Drawer } from 'antd';
-import { FilterOutlined } from '@ant-design/icons';
+import { List, Layout, message } from 'antd';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Beer } from './Beer';
 import "./style.scss";
 import { Filters } from './Filters';
 
-const { Title } = Typography;
-const { Header, Content, Footer } = Layout;
+const { Content, Footer } = Layout;
 
 const Beers = () => {
 
   const [beers, setBeers] = useState([]);
-  const [visible, setVisible] = useState(false);
+  const [filter, setFilter] = useState({});
 
   useEffect(() => {
-    axios.get('/beers')
+    axios.get('/beers', {
+      params: {
+        origin: filter.origin,
+        brewery: filter.brewery,
+        style: filter.style
+      }
+    })
     .then((response) => {
       setBeers(response.data);
     }).catch((error) => {
       message.error(error.statusText)
     });
-  }, []);
+  }, [filter]);
 
   return (
     <div className="site-drawer-render-in-current-wrapper">
       <Layout>
-        <Header className="beers-header">
-          <Row>
-            <Col span={20}>
-              <Title className="beers-title" level={1}>Cervezas</Title>
-            </Col>
-            <Col span={4}>
-                <Button type="primary" onClick={() => setVisible(true)}>Filter <FilterOutlined /></Button>
-            </Col>
-          </Row>
-          <Drawer
-            title="Filters"
-            placement="top"
-            closable={false}
-            onClose={() => setVisible(false)}
-            visible={visible}
-            height={150}
-            getContainer={false}
-            style={{ position: 'absolute' }}
-          >
-            <Filters />
-          </Drawer>
-        </Header>
+        <Filters onSearch={setFilter}/>
         <Content className="beers-content">
           <List
             grid={{
--- src/components/Beers/Filters.jsx
+++ src/components/Beers/Filters.jsx
@@ -1,82 +1,155 @@
-import { Select, Row, Col } from 'antd';
-import React from 'react';
+import { Select, Row, Col, Typography, Drawer, Button, Layout, message } from 'antd';
+import { FilterOutlined } from '@ant-design/icons';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
+const { Title } = Typography;
+const { Header } = Layout;
 
-const Filters = () => {
+const Filters = (props) => {
 
   const { Option } = Select;
+  const [visible, setVisible] = useState(false);
+  const [filterBrewery, setBrewery] = useState([]);
+  const [filterOrigin, setOrigin] = useState([]);
+  const [filterStyle, setStyle] = useState([]);
+  const [breweryOpts, setBrwOpts] = useState([]);
+  const [originOpts, setOrigOpts] = useState([]);
+  const [styleOpts, setStyleOpts] = useState([]);
+
+  useEffect(() => {
+    axios.get('/beers/distinct', {
+      params: {
+        value: "brewery"
+      }
+    })
+    .then((response) => {
+      setBrwOpts(response.data);
+    }).catch((error) => {
+      message.error(error.statusText)
+    });
+    axios.get('/beers/distinct', {
+      params: {
+        value: "origin"
+      }
+    })
+    .then((response) => {
+      setOrigOpts(response.data);
+    }).catch((error) => {
+      message.error(error.statusText)
+    });
+    axios.get('/beers/distinct', {
+      params: {
+        value: "style"
+      }
+    })
+    .then((response) => {
+      setStyleOpts(response.data);
+    }).catch((error) => {
+      message.error(error.statusText)
+    });
+  }, []);
 
   function onChangeBrewery(value) {
-    console.log(`brewery selected ${value}`);
+    setBrewery(value);
   }
 
   function onChangeOrigin(value) {
-    console.log(`origin selected ${value}`);
+    setOrigin(value);
   }
 
   function onChangeStyle(value) {
-    console.log(`style selected ${value}`);
+    setStyle(value)
+  }
+
+  function onClose() {
+    const filter = {
+      origin: filterOrigin,
+      brewery: filterBrewery,
+      style: filterStyle
+    }
+    props.onSearch(filter);
+    setVisible(false)
   }
 
   return(
-    <Row>
-      <Col span={8}>
-        Brewery
-        <Select
-          showSearch
-          style={{ width: 225, marginLeft: 10 }}
-          mode={"multiple"}
-          placeholder="Select a brewery"
-          optionFilterProp="children"
-          onChange={onChangeBrewery}
-          filterOption={(input, option) =>
-            option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-          }
-        >
-          <Option value="jack">Jack</Option>
-          <Option value="lucy">Lucy</Option>
-          <Option value="tom">Tom</Option>
-          <Option value="diego">Diego</Option>
-          <Option value="fer">Fer</Option>
-        </Select>
-      </Col>
-      <Col span={8}>
-        Origin
-        <Select
-          showSearch
-          style={{ width: 225, marginLeft: 10 }}
-          mode={"multiple"}
-          placeholder="Select a city"
-          optionFilterProp="children"
-          onChange={onChangeOrigin}
-          filterOption={(input, option) =>
-            option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-          }
-        >
-          <Option value="jack">Jack</Option>
-          <Option value="lucy">Lucy</Option>
-          <Option value="tom">Tom</Option>
-        </Select>
-      </Col>
-      <Col span={8}>
-        Style
-        <Select
-          showSearch
-          style={{ width: 225, marginLeft: 10 }}
-          mode={"multiple"}
-          placeholder="Select a style"
-          optionFilterProp="children"
-          onChange={onChangeStyle}
-          filterOption={(input, option) =>
-            option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-          }
-        >
-          <Option value="jack">Jack</Option>
-          <Option value="lucy">Lucy</Option>
-          <Option value="tom">Tom</Option>
-        </Select>
-      </Col>
-    </Row>
+    <Header className="beers-header">
+      <Row>
+        <Col span={20}>
+          <Title className="beers-title" level={1}>Cervezas</Title>
+        </Col>
+        <Col span={4}>
+            <Button type="primary" onClick={() => setVisible(true)}>Filter <FilterOutlined /></Button>
+        </Col>
+      </Row>
+      <Drawer
+        title="Filters"
+        placement="top"
+        closable={false}
+        onClose={onClose}
+        visible={visible}
+        height={150}
+        getContainer={false}
+        style={{ position: 'absolute' }}
+      >
+        <Row>
+          <Col span={8}>
+            Brewery
+            <Select
+              showSearch
+              style={{ width: 250, marginLeft: 10 }}
+              mode={"multiple"}
+              placeholder="Select a brewery"
+              optionFilterProp="children"
+              onChange={onChangeBrewery}
+              filterOption={(input, option) =>
+                option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+              }
+            >
+              {breweryOpts.map(item => (
+                <Option key={item}>{item}</Option>
+              ))}
+            </Select>
+          </Col>
+          <Col span={8}>
+            Origin
+            <Select
+              showSearch
+              style={{ width: 250, marginLeft: 10 }}
+              mode={"multiple"}
+              placeholder="Select a city"
+              optionFilterProp="children"
+              onChange={onChangeOrigin}
+              filterOption={(input, option) =>
+                option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+              }
+            >
+              {originOpts.map(item => (
+                <Option key={item}>{item}</Option>
+              ))}
+            </Select>
+          </Col>
+          <Col span={8}>
+            Style
+            <Select
+              showSearch
+              style={{ width: 250, marginLeft: 10 }}
+              mode={"multiple"}
+              placeholder="Select a style"
+              optionFilterProp="children"
+              onChange={onChangeStyle}
+              filterOption={(input, option) =>
+                option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+              }
+            >
+              {styleOpts.map(item => (
+                <Option key={item}>{item}</Option>
+              ))}
+            </Select>
+          </Col>
+        </Row>
+      </Drawer>
+    </Header>
   );
 };
 
